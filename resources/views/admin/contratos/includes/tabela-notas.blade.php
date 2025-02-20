d63d3ef51a96a078c7fddf4554768f0a470f951b
Rafael Lima <rafaelnlima@live.com>
2025-02-20 08:08:27 -0300

    atulização aprensentação de imagens notas

--- resources/views/admin/contratos/includes/tabela-notas.blade.php
+++ resources/views/admin/contratos/includes/tabela-notas.blade.php
@@ -4,18 +4,17 @@
     <div class="col-md-12">
         @foreach($contrato->historicos as $historico)
             @foreach($historico->notas as $nota)
-                <div style="border-bottom: 1px solid #adadad">
+                <div style="border: 1px solid #e2e2e2; padding: 5px; margin: 10px">
 
 
                     <div class="row " >
-                        <div class="col-md-3">
-                            <h5>{{$nota->historico->status->nome. " - ".\Carbon\Carbon::parse($nota->created_at)->format('d/m/Y')}}</h5>
-                            <h6>{{$nota->tipo->nome}}</h6>
-                            <a class="btn btn-warning" href="{{route('contrato.editar.nota',['id'=>$contrato->id,'historico_id'=>$historico->id,'nota_id'=>$nota->id])}}"> editar</a>
-                        </div>
-                        <div class="col-md-9">
+                        <div class="col-md-12">
+                            <h4>{{$nota->historico->status->nome. " - ".$nota->tipo->nome." - ".\Carbon\Carbon::parse($nota->created_at)->format('d/m/Y')}}</h4>
                             {!! $nota->texto !!}
+                            <a style="margin: 10px" class="btn btn-sm btn-warning" href="{{route('contrato.editar.nota',['id'=>$contrato->id,'historico_id'=>$historico->id,'nota_id'=>$nota->id])}}"> editar</a>
+
                         </div>
+
                     </div>
                     @if($nota->imagens()->count() != 0)
                         <div class="galeriaNotas gallery">
