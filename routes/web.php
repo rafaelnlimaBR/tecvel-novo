e18e229bddd136a694662163f26a78170ac66eff
Rafael Lima <rafaelnlima@live.com>
2025-05-02 12:03:48 -0300

    testando zaap

--- routes/web.php
+++ routes/web.php
@@ -253,7 +253,7 @@ Route::get('/teste',function () {
 });
 
 Route::get('teste-api',function () {
-
+    return env('URL_EVOLUTIONAPI').' '.env('INSTANCE_EVOLUTIONAPI').' '.env('URL_EVOLUTIONAPI');
     $curl = curl_init();
 
     curl_setopt_array($curl, [
